f7e1cbc927102c803d522d383eb8c62a3ef2e355
Fatima8100 <fatimaahmed04478@gmail.com>
2025-09-09 18:11:14 +0500

    Update task5_pascal.h

--- task5_pascal.h
+++ task5_pascal.h
@@ -1,6 +1,19 @@
 #include "task5_pascal.h"
+#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
 
-// Implementation will be added here
-std::vector<std::vector<int>> pascalTriangle(int n) {
-    return {}; // temporary placeholder
+// Function to generate Pascal's Triangle
+vector<vector<int>> generatePascal(int n) {
+    vector<vector<int>> triangle(n);
+
+    for (int i = 0; i < n; i++) {
+        triangle[i].resize(i + 1);
+        triangle[i][0] = triangle[i][i] = 1;
+        for (int j = 1; j < i; j++) {
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+    }
+    return triangle;
 }
